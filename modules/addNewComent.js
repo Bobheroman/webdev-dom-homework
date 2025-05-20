b14811a6b7dcc2dc4db3c1a4930db73309dc5bc5
Bobheroman <bobheroman@gmail.com>
2025-05-20 19:59:26 +0300

    Check my homework

--- modules/addNewComent.js
+++ modules/addNewComent.js
@@ -12,28 +12,56 @@ export function addComment(name, text) {
         isLiked: false,
         text: text.value,
         name: name.value,
+        forceError: true,
     }
     formEl.classList.add('loader')
-    const loaderCommentsEl = document.createElement('div');
-    loaderCommentsEl.classList.add('loader-comments');
+    const loaderCommentsEl = document.createElement('div')
+    loaderCommentsEl.classList.add('loader-comments')
     loaderCommentsEl.textContent = 'Комментарий добавляется...'
     const comentsEl = document.querySelector('.comments')
-    comentsEl.append(loaderCommentsEl);
-    
+    comentsEl.append(loaderCommentsEl)
 
     fetch('https://wedev-api.sky.pro/api/v1/:Bobheroman/comments', {
         method: 'POST',
         body: JSON.stringify(newComment),
     })
-        .then(() => {
+        .then((response) => {
+            const responseStatus = response.status
+            if (responseStatus === 500) {
+                throw new Error('Сервер сломался, попробуй позже')
+            }
+            if (responseStatus === 400) {
+                if (nameEl.value.length < 3) {
+                    name.classList.add('error')
+                } else {
+                    nameEl.classList.remove('error')
+                }
+
+                if (textEl.value.length < 3) {
+                    text.classList.add('error')
+                } else {
+                    textEl.classList.remove('error')
+                }
+
+                throw new Error(
+                    'Имя и комментарий должны быть не короче 3х символов',
+                )
+            }
             return fetchAndRenderComments()
         })
         .then(() => {
-            formEl.classList.remove('loader')
-            loaderCommentsEl.remove()
             nameEl.classList.remove('error')
             textEl.classList.remove('error')
         })
-
-    
+        .catch((error) => {
+            if (error.message === 'Сервер сломался, попробуй позже') {
+                addComment(name, text)
+            } else {
+                alert(error.message)
+            }
+        })
+        .finally(() => {
+            formEl.classList.remove('loader')
+            loaderCommentsEl.remove()
+        })
 }
